05b5579fd58b80b70e71db5e7fd8495c1fdac301
May, Nicole [USA] <May_Nicole@bah.com>
2020-06-02 21:06:46 -0400

    adding a header molecule

--- project-resume/src/pages/homepage/Homepage.jsx
+++ project-resume/src/pages/homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { NavButton } from '../../atoms/NavButton/NavButton';
+import { Header } from '../../molecules/header/Header'
 import './homepage.css';
 
 export default class Homepage extends Component{
@@ -9,34 +10,37 @@ export default class Homepage extends Component{
     const { history } = this.props;
 
     return (
-      <div className="homepage-outer-flexbox">
-        <div className="homepage-row-one">
-          <NavButton 
-            icon="timeline"
-            history={history}
-          />
-          <NavButton 
-            icon="me" 
-            history={history}
-          />
-          <NavButton 
-            icon="learn" 
-            history={history}
-          />
-        </div>
-        <div className="homepage-row-two">
-          <NavButton 
-            icon="meta" 
-            history={history}
-          />
-          <NavButton 
-            icon="projects" 
-            history={history}
-          />
-          <NavButton 
-            icon="education" 
-            history={history}
-          />
+      <div className="full-page">
+        <Header primaryTitle="Nicole May" secondaryTitle="Interactive Resume"/>
+        <div className="homepage-outer-flexbox">
+          <div className="homepage-row-one">
+            <NavButton 
+              icon="timeline"
+              history={history}
+            />
+            <NavButton 
+              icon="me" 
+              history={history}
+            />
+            <NavButton 
+              icon="learn" 
+              history={history}
+            />
+          </div>
+          <div className="homepage-row-two">
+            <NavButton 
+              icon="meta" 
+              history={history}
+            />
+            <NavButton 
+              icon="projects" 
+              history={history}
+            />
+            <NavButton 
+              icon="education" 
+              history={history}
+            />
+          </div>
         </div>
       </div>
     );
