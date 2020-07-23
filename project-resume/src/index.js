8150f77ae9651e92b3e640ea62d8b6f5140ca912
May, Nicole [USA] <May_Nicole@bah.com>
2020-07-23 15:52:04 -0400

    adding the base name for prod and local

--- project-resume/src/index.js
+++ project-resume/src/index.js
@@ -5,16 +5,21 @@ import * as serviceWorker from './serviceWorker';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage';
 import Timeline from './pages/timeline/Timeline';
+import Me from './pages/me/Me';
 import Footer from './organisms/footer/Footer';
-import ContentContainer from './pages/content/ContentContainer';
+import global from './global.json';
 
 ReactDOM.render(
     <div>
-        <BrowserRouter>
+        <BrowserRouter basename={global.isProd ? "https://imaybeniki.github.io/" : "http://localhost:3000"}>
             <Switch>
-                <Route path="/home" component={Homepage} />
                 <Route path="/timeline" component={Timeline} />
-                <Route path="*" component={ContentContainer} />
+                <Route path="/me" component={Me} />
+                <Route path="/projects" component={Homepage} />
+                <Route path="/education" component={Homepage} />
+                <Route path="/learn" component={Homepage} />
+                <Route path="/meta" component={Homepage} />
+                <Route path="*" component={Homepage} />
             </Switch>
         </BrowserRouter>
         <Footer />
