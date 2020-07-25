24e8b71cbd4b31f2ee26024188f11ee0b8ac8ef8
May, Nicole [USA] <May_Nicole@bah.com>
2020-07-25 11:41:24 -0400

    setting up provider and redux

--- project-resume/src/index.js
+++ project-resume/src/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import createStore from 'redux';
+import { createBrowserHistory } from 'history';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage';
 import Timeline from './pages/timeline/Timeline';
 import Footer from './organisms/footer/Footer';
 import ContentContainer from './pages/content/ContentContainer';
 
+const store = createStore();
+const history = createBrowserHistory();
+
 ReactDOM.render(
     <div>
-        <BrowserRouter basename={global.isProd ? "https://imaybeniki.github.io/" : "http://localhost:3000"}>
-            <Switch>
+        <Provider store={ store }>
+            <Router history={ history }  >
                 <Route path="/content/{type}" component={ContentContainer} />
                 <Route path="/timeline" component={Timeline} />
                 <Route path="*" component={Homepage} />
-            </Switch>
-        </BrowserRouter>
+            </Router>
+        </Provider>
         <Footer />
     </div>, 
     document.getElementById('root'));
