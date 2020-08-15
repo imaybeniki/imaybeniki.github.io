f24e4d9cb91fd07a17ef3ea26af765561d671d45
May, Nicole [USA] <May_Nicole@bah.com>
2020-08-15 17:33:52 -0400

    clean up

--- project-resume/src/molecules/content/IconContent.jsx
+++ project-resume/src/molecules/content/IconContent.jsx
@@ -6,25 +6,40 @@ import Icons from "../icons/Icons";
 
 /**
  * @summary The homepage renders specified icons into two flexbox rows
- * @property {string} title: the title of the content
  * @property {string} body: the string text representing the content
+ * @property {string} history: the React Router history object
+ * @property {string} iconName: the name of the icon to render
+ * @property {string} title: the title of the content
  */
-export default function IconContent(props){
-  const { body, title } = props;
+export default function IconContent(
+  { 
+    body, 
+    history,
+    iconName,
+    title,
+  }
+){
 
-  return (
-    <div className="outer-content-box">
-        <Title title={title} />
-        <div className="content-box">
-            <Icons icons={["aspen"]}/>
-            <p>{body}</p>
-        </div>
-    </div>
-  );
+  return iconName 
+    ? (
+      <div className="outer-content-box">
+          <Title title={title} />
+          <div className="content-box">
+              <Icons 
+                icons={[iconName || "me"]}
+                history={history}
+              />
+              <p>{body}</p>
+          </div>
+      </div>
+    ):
+    <React.Empty />
 }
 
 IconContent.propTypes = {
   body: PropTypes.string,
+  history: PropTypes.object.isRequired,
+  iconName: PropTypes.string.isRequired,
   title: PropTypes.string,
 }
 
--- project-resume/src/organisms/content/ContentFilter.jsx
+++ project-resume/src/organisms/content/ContentFilter.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import ShortContent from '../../molecules/content/VideoContent';
-import JournalContent from '../../molecules/content/JournalContent';
 import Content from '../../molecules/content/Content';
+import IconContent from '../../molecules/content/IconContent';
+import JournalContent from '../../molecules/content/JournalContent';
+import ShortContent from '../../molecules/content/VideoContent';
 import * as contentOperations from '../../content/contentOperations';
 
 /**
  * @summary Renders an array of Content boxes from an array of JSON objects
- * @property {array} contentList: a groomed array of content to be rendered
- * @property {string} title: 
+ * @property {string} history: React Router history object
  */
 export default function ContentFilter(props){
   // Get the history. This will contain the URL which includes category
   const category = props.location.pathname
+  const { history } = props;
 
   // Get the list of content from content folder
   const contentList = contentOperations.getContentList();
@@ -21,11 +22,32 @@ export default function ContentFilter(props){
   // Render content boxes based on their types
   return contentToRender.map((content) => {
         if (content.type === 'short'){
-            return <ShortContent title={content.title} body={content.body}/>
+            return <ShortContent 
+                      title={content.title} 
+                      body={content.body}
+                      key={content.date}
+                    />
         } 
         if (content.type === 'journal'){
-            return <JournalContent  title={content.title} body={content.body}/>
+            return <JournalContent  
+                      title={content.title} 
+                      body={content.body}
+                      key={content.date}
+                    />
         }
-        return <Content title={content.title} body={content.body} key={content.date}/>
+        if (content.type === 'icon'){
+          return <IconContent 
+                    title={content.title} 
+                    body={content.body} 
+                    iconName={content.iconName || 'me'}
+                    history={history}
+                    key={content.date}
+                  />
+        }
+        return <Content 
+                  title={content.title} 
+                  body={content.body} 
+                  key={content.date}
+                />
   });
 }
